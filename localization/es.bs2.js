21778eeafb87b2efb8645bf65ac421d3e855f846
JaBO <admin@jaboweb.com>
2015-11-23 20:10:28 +0100

    add SPANISH localization bootstrap v2

---
+++ localization/es.bs2.js
@@ -0,0 +1,32 @@
+/**
+ * bs_grid v0.9.2 localization - SPANISH (bootstrap 2 version) *****************
+ *
+ * DO NOT CHANGE this file, as it will be overwritten in next update.
+ * To use different values, write and use a similar structure js file.
+ *
+ */
+var rsc_bs_dg = {
+
+    columns: "Columnas",
+    columns_show_row_numbers: "Número de linas",
+    columns_default: "Defecto",
+
+    sorting: "Orden",
+    sort_ascending: "<i class='icon-chevron-up'></i>&nbsp;&nbsp;",
+    sort_descending: "<i class='icon-chevron-down'></i>&nbsp;&nbsp;",
+    sort_none: "<i class='icon-minus'></i>&nbsp;&nbsp;",
+    sorting_default: "Defecto",
+
+    filters: "Filtros",
+    filters_apply: "Aplicar",
+    filters_reset: "Reset",
+
+    select: "Seleccionar",
+    select_all_in_page: "Todos",
+    deselect_all_in_page: "Ninguno",
+    select_inverse_in_page: "Invertir",
+    deselect_all: "Deseleccionar todos",
+
+    row_index_header: "#",
+    no_records_found: "Sin resultados"
+};
