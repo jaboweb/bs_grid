12671262f25a0f54f482f0fa76d7b2594e0d3262
JaBO <admin@jaboweb.com>
2015-11-23 19:51:12 +0100

    add localization SPANISH

---
+++ localization/es.js
@@ -0,0 +1,36 @@
+/**
+ * bs_grid v0.9.2 localization - SPANISH ***************************************
+ *
+ * DO NOT CHANGE this file, as it will be overwritten in next update.
+ * To use different values, write and use a similar structure js file.
+ *
+ */
+var rsc_bs_dg = {
+
+    columns: "Columnas",
+    columns_show_row_numbers: "Número de linas",
+    columns_default: "Defecto",
+
+    sorting: "Orden",
+    sort_ascending: "<i class='glyphicon glyphicon-chevron-up'></i>&nbsp;&nbsp;",
+    sort_descending: "<i class='glyphicon glyphicon-chevron-down'></i>&nbsp;&nbsp;",
+    sort_none: "<i class='glyphicon glyphicon-minus'></i>&nbsp;&nbsp;",
+    sorting_default: "Defecto",
+
+    print: "Crear PDF",
+    export: "Exportar a Excel",
+
+    filters: "Filtros",
+    filters_apply: "Aplicar",
+    filters_reset: "Borrar",
+    filters_close: "Cerrar",
+
+    select: "Seleccionar",
+    select_all_in_page: "Todos",
+    deselect_all_in_page: "Ninguno",
+    select_inverse_in_page: "Invertir",
+    deselect_all: "Deseleccionar todos",
+
+    row_index_header: "#",
+    no_records_found: "Sin resultados"
+};
